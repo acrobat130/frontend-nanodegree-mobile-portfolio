e6c4bd3fb139ff1b0f678e42a9ce7b66c67647a3
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-11-23 17:09:31 -0800

    moved variables back into updatePositions function

--- views/js/main.js
+++ views/js/main.js
@@ -519,13 +519,13 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
-var scrollTop = document.body.scrollTop;
-var items = document.querySelectorAll('.mover');
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  var items = document.querySelectorAll('.mover');
   // moved reference to document body outside of loop
+  var scrollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
